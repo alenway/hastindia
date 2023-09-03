094c8704103f58d4a6509a53704b699a67c6a253
narendra <51484021+alenway@users.noreply.github.com>
2023-09-03 11:29:34 +0530

    home page

---
+++ src/components/Home.tsx
@@ -0,0 +1,106 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+
+      <section className="text-gray-600 body-font">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+              Before they sold out
+              <br className="hidden lg:inline-block" />
+              readymade gluten
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              Copper mug try-hard pitchfork pour-over freegan heirloom neutra air
+              plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk
+              tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard
+              chambray.
+            </p>
+            <div className="flex justify-center">
+              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                Button
+              </button>
+              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                Button
+              </button>
+            </div>
+          </div>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img
+              className="object-cover object-center rounded"
+              alt="hero"
+              src="https://dummyimage.com/720x600"
+            />
+          </div>
+        </div>
+      </section>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto flex flex-wrap">
+          <div className="flex w-full mb-20 flex-wrap">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4">
+              Master Cleanse Reliac Heirloom
+            </h1>
+            <p className="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">
+              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+              gentrify, subway tile poke farm-to-table. Franzen you probably haven't
+              heard of them man bun deep jianbing selfies heirloom.
+            </p>
+          </div>
+          <div className="flex flex-wrap md:-m-2 -m-1">
+            <div className="flex flex-wrap w-1/2">
+              <div className="md:p-2 p-1 w-1/2">
+                <img
+                  alt="gallery"
+                  className="w-full object-cover h-full object-center block"
+                  src="https://dummyimage.com/500x300"
+                />
+              </div>
+              <div className="md:p-2 p-1 w-1/2">
+                <img
+                  alt="gallery"
+                  className="w-full object-cover h-full object-center block"
+                  src="https://dummyimage.com/501x301"
+                />
+              </div>
+              <div className="md:p-2 p-1 w-full">
+                <img
+                  alt="gallery"
+                  className="w-full h-full object-cover object-center block"
+                  src="https://dummyimage.com/600x360"
+                />
+              </div>
+            </div>
+            <div className="flex flex-wrap w-1/2">
+              <div className="md:p-2 p-1 w-full">
+                <img
+                  alt="gallery"
+                  className="w-full h-full object-cover object-center block"
+                  src="https://dummyimage.com/601x361"
+                />
+              </div>
+              <div className="md:p-2 p-1 w-1/2">
+                <img
+                  alt="gallery"
+                  className="w-full object-cover h-full object-center block"
+                  src="https://dummyimage.com/502x302"
+                />
+              </div>
+              <div className="md:p-2 p-1 w-1/2">
+                <img
+                  alt="gallery"
+                  className="w-full object-cover h-full object-center block"
+                  src="https://dummyimage.com/503x303"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+
+  )
+}
+
+export default Home
